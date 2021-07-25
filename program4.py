23566360824a3b20287275a4f520ee686738dce8
macromrit <77322873+macromrit@users.noreply.github.com>
2021-07-25 09:26:04 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,7 +1,7 @@
 # # #program 4 - sorting a dictionary
 # #wap to sort a dictionary by key
 
-x = {"ariana_grande": 250, "justing_bieber": 200e+100, 'lady_gaga': 100, 'dj_snake': 100e+12}
+x = {"ariana_grande": 250, "justin_bieber": 200e+100, 'lady_gaga': 100, 'dj_snake': 100e+12}
 
 y = sorted(x.keys())
 
