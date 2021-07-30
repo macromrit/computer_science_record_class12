bb79a59b13c5c24f437c548d466fb1669954c2f8
macromrit <77322873+macromrit@users.noreply.github.com>
2021-07-30 14:31:00 +0530

    Create app.py

---
+++ program6/app.py
@@ -0,0 +1,7 @@
+from modules import *
+
+cube = float(input("Enter the length of side of a cube in cm's to find its volume: "))
+print(vol_cube(cube))
+
+sphere = float(input("Enter the radius of a sphere in cm's to find its volume: "))
+print(vol_sphere(sphere))
