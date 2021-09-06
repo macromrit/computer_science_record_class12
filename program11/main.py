08784289c250bd046c4bea102da9caae79554fe5
macromrit <77322873+macromrit@users.noreply.github.com>
2021-09-06 15:09:43 +0530

    Update main.py

--- program11/main.py
+++ program11/main.py
@@ -1,2 +1,9 @@
-with open('program11.txt', 'r') as blueorigin:
-    print('#'.join(blueorigin.read().split()))
+def hashprint(filename: str)->str:
+    with open(filename, 'r') as blueorigin:
+        print('#'.join(blueorigin.read().split()))
+hashprint('program11.txt')
+
+'''
+output->
+Jeff#flew#to#space#and#is#back#again#alive#XD
+'''  
